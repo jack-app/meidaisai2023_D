713eedb43001cd2d882268d7a90050731988b554
mono-1729 <kawai.172957@gmail.com>
2023-05-16 12:33:38 +0900

    phaser.js導入

---
+++ public/scripts/main.js
@@ -0,0 +1,26 @@
+import { StartScene } from "./scenes/StartScene.js"
+import { PlayScene } from "./scenes/PlayScene.js"
+import { ResultScene } from "./scenes/ResultScene.js"
+
+
+
+const config = {
+    parent: 'mainFrame',
+    type: Phaser.AUTO,
+    height: 600,
+    width: 800,
+    scene: [StartScene, PlayScene, ResultScene]
+}
+
+
+class Game extends Phaser.Game {
+    constructor(config) {
+        super(config)
+    }
+}
+
+
+
+window.addEventListener("load", () => {
+    const game = new Game(config)
+})
---
+++ public/scripts/scenes/PlayScene.js
@@ -0,0 +1,38 @@
+
+export class PlayScene extends Phaser.Scene {
+    constructor() {
+        super({ key: "PlayScene" })
+    }
+
+
+    init() {
+        // Can be defined on your own Scenes.
+        // This method is called by the Scene Manager when the scene starts, before preload() and create().
+    }
+
+
+    preload() {
+        // Can be defined on your own Scenes. Use it to load assets.
+        // This method is called by the Scene Manager, after init() and before create(), only if the Scene has a LoaderPlugin.
+        // After this method completes, if the LoaderPlugin's queue isn't empty, the LoaderPlugin will start automatically
+    }
+
+
+    create() {
+        // Can be defined on your own Scenes. Use it to create your game objects.
+        // This method is called by the Scene Manager when the scene starts, after init() and preload().
+        // If the LoaderPlugin started after preload(), then this method is called only after loading is complete.
+        const sceneName = this.add.text(150, 70, 'PlayScene').setFontSize(30).setFontFamily("Arial").setOrigin(0.5).setInteractive();
+
+	    const change = this.add.text(150, 130, 'Change Scene!').setFontSize(20).setFontFamily("Arial").setOrigin(0.5).setInteractive();
+
+        change.on('pointerdown', function (pointer) {
+            this.scene.start('ResultScene');
+        }, this);
+    }
+
+
+    update() {
+        // https://photonstorm.github.io/phaser3-docs/Phaser.Scene.html#update
+    }
+}
