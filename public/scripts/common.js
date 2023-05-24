a0b161f9c98b99408ce722c014a50e0e7f558228
simasannnn <kotaro20041212@gmail.com>
2023-05-24 18:22:50 +0900

    createb yaku

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -2,13 +2,13 @@
 //     console.log(Judgeyaku([100]));        //{yakus:[],mon:0}
 //     ※役がない場合はyakus:[],mon:0
 // Playscene.jsにデバッグ用コードあるよ
-export const YAKU={1:'五光',2:'四光',3:'雨四光',4:'三光',5:'花見酒',6:'月見酒',7:'猪鹿蝶',8:'赤短',9:'青短',10:'タネ',11:'タン',12:'カス'}/ここで役の一覧を管理すると分かりやすいかも
+export const YAKU={1:'五光',2:'四光',3:'雨四光',4:'三光',5:'花見酒',6:'月見酒',7:'猪鹿蝶',8:'赤短',9:'青短',10:'タネ',11:'タン',12:'カス'}//ここで役の一覧を管理すると分かりやすいかも
 
-export function Judgeyaku(hand){
-	let hand =new Set(hand);
+export function Judgeyaku(handlist){
+	let hand =new Set(handlist);
 	let yaku={yakus:[],mon:0};
 	
-	if(100 in hand && 300 in hand && 800 in hand){
+	if(hand.has(100) && hand.has(300) && hand.has(800)){
 		yaku.yakus.push('三光');
 		yaku.mon+=5;
 	}
