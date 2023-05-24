635208aa6b90a1e5a8b7a92aee234be7bb5cf441
simasannnn <kotaro20041212@gmail.com>
2023-05-24 18:09:43 +0900

    create yaku

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -5,44 +5,20 @@
 export const YAKU={1:'五光',2:'四光',3:'雨四光',4:'三光',5:'花見酒',6:'月見酒',7:'猪鹿蝶',8:'赤短',9:'青短',10:'タネ',11:'タン',12:'カス'}/ここで役の一覧を管理すると分かりやすいかも
 
 export function Judgeyaku(hand){
-	yaku={yakus:[],mon:0};
+	let hand =new Set(hand);
+	let yaku={yakus:[],mon:0};
+	
+	if(100 in hand && 300 in hand && 800 in hand){
+		yaku.yakus.push('三光');
+		yaku.mon+=5;
+	}
+	
+	
+	
+	
+	
 	return yaku;
 
-	yaku={yakus:[{num:1,name:'五光'}],mon:10};
-	return yaku;
-
-	yaku={yakus:[{num:2,name:'四光'}],mon:8};
-	return yaku;
-
-	yaku={yakus:[{num:3,name:'雨四光'}],mon:7};
-	return yaku;
-
-	yaku={yakus:[{num:4,name:'三光'}],mon:5};
-	return yaku;
-
-	yaku={yakus:[{num:5,name:'花見酒'}],mon:5};
-	return yaku;
-
-	yaku={yakus:[{num:6,name:'月見酒'}],mon:5};
-	return yaku;
-
-	yaku={yakus:[{num:7,name:'猪鹿蝶'}],mon:5};
-	return yaku;
-
-	yaku={yakus:[{num:8,name:'赤短'}],mon:5};
-	return yaku;
-
-	yaku={yakus:[{num:9,name:'青短'}],mon:5};
-	return yaku;
-
-	yaku={yakus:[{num:10,name:'タネ'}],mon:1};
-	return yaku;
-
-	yaku={yakus:[{num:11,name:'タン'}],mon:1};
-	return yaku;
-
-	yaku={yakus:[{num:12,name:'カス'}],mon:1};
-	return yaku;
-
+	
 }
 
