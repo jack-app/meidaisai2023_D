11657a0b3551434d769c12bd8d61a452161a7fd1
anchan2002 <kukki00427@gmail.com>
2023-05-24 23:14:12 +0900

    resultscene色わからなかった

--- public/scripts/scenes/ResultScene.js
+++ public/scripts/scenes/ResultScene.js
@@ -14,6 +14,7 @@ export class ResultScene extends Phaser.Scene {
         // Can be defined on your own Scenes. Use it to load assets.
         // This method is called by the Scene Manager, after init() and before create(), only if the Scene has a LoaderPlugin.
         // After this method completes, if the LoaderPlugin's queue isn't empty, the LoaderPlugin will start automatically
+        this.load.image("background", "./images/background.png");
     }
 
 
@@ -28,6 +29,23 @@ export class ResultScene extends Phaser.Scene {
         change.on('pointerdown', function (pointer) {
             this.scene.start('StartScene');
         }, this);
+
+        this.add.image(400,330, "background");
+        
+        let graphics = this.add.graphics();
+        graphics.fillStyle(999999,90).fillRect(80, 130, 650, 400);
+
+        this.add.text(90 , 140, "RESULT", {fontSize: 30,fontFamily: "impact"});
+        this.add.text(160, 230, "VICTORY!!", {fontSize: 70,fontFamily: "fantasy"});
+        this.add.text(450, 260, "congratulations", {fontSize: 30,fontFamily: "Arial"});
+        this.add.text(150, 350, "score", {fontSize: 30,fontFamily: "cursive"});
+        this.add.text(150, 400, "people", {fontSize: 30,fontFamily: "cursive"});
+        this.add.text(300, 360, "対", {fontSize: 30,fontFamily: "Arial"});
+        this.add.text(300, 400, "対", {fontSize: 30,fontFamily: "Arial"});
+
+        this.add.line(0,120,1000,200,400,200,0xff0000);//line
+
+        
     }
 
 
