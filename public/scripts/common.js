dc7c8daec0cdbc482a739d9b8c6b8381e54e8ace
simasannnn <kotaro20041212@gmail.com>
2023-05-24 10:32:51 +0900

    create yaku

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -2,9 +2,10 @@
 //     console.log(Judgeyaku([100]));        //{yakus:[],mon:0}
 //     ※役がない場合はyakus:[],mon:0
 // Playscene.jsにデバッグ用コードあるよ
-export const YAKU={1:'三光',}//ここで役の一覧を管理すると分かりやすいかも
+export const YAKU={1:'五光',2:'四光',3:'雨四光',4:'三光',5:'花見酒',6:'月見酒',7:'猪鹿蝶',8:'赤短',9:'青短',10:'タネ',11:'タン',12:'カス'}/ここで役の一覧を管理すると分かりやすいかも
 
 export function Judgeyaku(hand){
 	yaku={yakus:[],mon:0};
 	return yaku;
 }
+
