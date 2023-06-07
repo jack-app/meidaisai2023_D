4fd847df8281cbc8b1f088c98926995b7f59734d
mono-1729 <kawai.172957@gmail.com>
2023-06-07 15:14:13 +0900

    同じ月の札を重ねる処理

--- public/scripts/scenes/PlayScene.js
+++ public/scripts/scenes/PlayScene.js
@@ -12,8 +12,6 @@ export class PlayScene extends Phaser.Scene {
         // This method is called by the Scene Manager when the scene starts, before preload() and create().
         // console.log(Judgeyaku([100,300,800])); //{yakus:[{num:1,name:'三光'}],mon:5}
         // console.log(Judgeyaku([100]));    //{yakus:[],mon:0}
-
-
         // console.log(Judgeyaku([120,220,320,420,520,620,720,920,1020,1120]));
         // console.log(Judgeyaku([120,220,320,420]));
         // console.log(Judgeyaku([120,220,320,420,520]));
@@ -156,7 +154,9 @@ export class PlayScene extends Phaser.Scene {
         this.enemy_got_cards = [[], [], [], []];
         this.deck = [];
         this.deck_list = CARDS;
-        while (true) {
+        this.shuffle = true;
+        while (this.shuffle) {
+            this.shuffle = false;
             for (let i = this.deck_list.length - 1; i >= 0; i--) {
                 let rand = Math.floor(Math.random() * (i + 1));
                 // 配列の要素の順番を入れ替える
@@ -164,19 +164,42 @@ export class PlayScene extends Phaser.Scene {
                 this.deck_list[i] = this.deck_list[rand];
                 this.deck_list[rand] = tmpStorage;
             }
-            for (let i = 0; i > 5; i--) {
-                let rand = Math.floor(Math.random() * (i + 1));
-                // 配列の要素の順番を入れ替える
-                let tmpStorage = this.deck_list[i];
-                this.deck_list[i] = this.deck_list[rand];
-                this.deck_list[rand] = tmpStorage;
+            for (let i = 0; i < 3; i++) {
+                this.month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+                for (let j = i * 8; j < 8 + i * 8; j++) {
+                    this.month[Math.floor(this.deck_list[j] / 100) - 1]++;
+                }
+                if (Math.max(...this.month) == 4) {
+                    this.shuffle = true;
+                }
             }
-            break;
+        }
+        let field_count = 0;
+        let field_pos = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
+        for (let i = 16; i < 24; i++) {
+            let month_num = Math.floor(this.deck_list[i] / 100) - 1;
+            if (this.month[month_num] == 3) {
+                if (field_pos[month_num] == -1) {
+                    this.field_cards[field_count].push(
+                        new Card(this, this.deck_list[i], 0)
+                    );
+                    field_pos[month_num] = field_count;
+                    field_count++;
+                } else {
+                    this.field_cards[field_pos[month_num]].push(
+                        new Card(this, this.deck_list[i], 0)
+                    );
+                }
+            } else {
+                this.field_cards[field_count].push(
+                    new Card(this, this.deck_list[i], 0)
+                );
+                field_count++;
+            }
         }
         for (let i = 0; i < 8; i++) {
             this.player_cards.push(new Card(this, this.deck_list[i], 0));
             this.enemy_cards.push(new Card(this, this.deck_list[8 + i], 0));
-            this.field_cards[i].push(new Card(this, this.deck_list[16 + i], 0));
         }
         for (let i = 0; i < 24; i++) {
             this.deck.push(new Card(this, this.deck_list[24 + i], 0));
