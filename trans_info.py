9c2331e7b2685eba1d453c630b176a6906933229
takakura-kazushi <Kazken.aries.munster@outlook.com>
2024-11-08 18:36:23 +0900

    情報をユニバース形式に変更する実装

--- trans_info.py
+++ trans_info.py
@@ -5,7 +5,7 @@ CARDS = [
     1130, 1200, 1230, 1231, 1232,
 ]
 
-def transInfo(observation): #observationとobservation２つを受け取る？
+def transInfo(observation): 
     UnivInfo = {
         'field_cards': [
             [],
@@ -26,35 +26,78 @@ def transInfo(observation): #observationとobservation２つを受け取る？
             [],
         ],
         'player_cards': [],    
-        'selected': -1,
+        # 'selected': -1, #手札で選択中のカード
         'player_got_cards': [[],[],[],[]],
         'enemy_cards': [],
         'enemy_cards_back': [], 
         'enemy_got_cards': [[], [], [], []],
-        'deck': [],
+        # 'deck': [], #山札
         'deck_list': CARDS,
-        'shuffle': True,
-        'select_time': False,
-        'deck_card': None,
+        'select_time': False, #自分のターンか
+        'deck_card': None, 
         'koikoi': False
     }
     
     # フィールドに初期の場札を配置
     for i in range(len(observation['field_card'])):
-        new_card_pos = observation['field_card'][i][0] - 1 * 4 + observation['field_card'][i][1] - 1  #月と強さからCARDS内の位置を求める
-        UnivInfo['field_cards'].append(CARDS[new_card_pos])
+        new_card_pos = (observation['field_card'][i][0] - 1 )* 4 + observation['field_card'][i][1] - 1  #月と強さからCARDS内の位置を求める
+        UnivInfo['field_cards'][observation['round'] - 1].append(CARDS[new_card_pos])
+    print(UnivInfo['field_cards'])
 
+    # 人間の持ち札
     for i in range(len(observation['your_hand'])):
-        new_card_pos = observation['your_hand'][i][0] - 1 * 4 + observation['your_hand'][i][1] - 1  
+        new_card_pos = (observation['your_hand'][i][0] - 1 )* 4 + observation['your_hand'][i][1] - 1  
         UnivInfo['player_cards'].append(CARDS[new_card_pos])
+    print(UnivInfo['player_cards'])
     
-    for i in range(len(observation['opponent'])):
-        new_card_pos = observation['opponent'][i][0] - 1 * 4 + observation['opponent'][i][1] - 1  
+    # AIの持ち札
+    for i in range(len(observation['opponent_hand'])):
+        new_card_pos = (observation['opponent_hand'][i][0] - 1 )* 4 + observation['opponent_hand'][i][1] - 1  
         UnivInfo['enemy_cards'].append(CARDS[new_card_pos])
+    print(UnivInfo['enemy_cards'])
 
-    
+    # ここにeneny_cards_backを書く
 
+    for i in range(len(observation['your_pile'])):
+        new_card_pos = (observation['your_pile'][i][0] - 1 )* 4 + observation['your_pile'][i][1] - 1
+        NewCard = CARDS[new_card_pos]
+        if NewCard % 100 // 10 == 0: #光札
+            UnivInfo['player_got_cards'][0].append(CARDS[new_card_pos])
+        if NewCard % 100 // 10 == 1: #種札
+            UnivInfo['player_got_cards'][1].append(CARDS[new_card_pos])
+        if NewCard % 100 // 10 == 2: #短冊札
+            UnivInfo['player_got_cards'][2].append(CARDS[new_card_pos])
+        if NewCard % 100 // 10 == 3: #素札
+            UnivInfo['player_got_cards'][3].append(CARDS[new_card_pos])
+    print(UnivInfo['player_got_cards'])
+
+    for i in range(len(observation['opponent_pile'])): #opponent?pileはobservationに存在しない？
+        new_card_pos = (observation['opponent_pile'][i][0] - 1 )* 4 + observation['opponent_pile'][i][1] - 1
+        NewCard = CARDS[new_card_pos]
+        if NewCard % 100 // 10 == 0: #光札
+            UnivInfo['enemy_got_cards'][0].append(CARDS[new_card_pos])
+        if NewCard % 100 // 10 == 1: #種札
+            UnivInfo['enemy_got_cards'][1].append(CARDS[new_card_pos])
+        if NewCard % 100 // 10 == 2: #短冊札
+            UnivInfo['enemy_got_cards'][2].append(CARDS[new_card_pos])
+        if NewCard % 100 // 10 == 3: #素札
+            UnivInfo['enemy_got_cards'][3].append(CARDS[new_card_pos])
+    print(UnivInfo['enemy_got_cards'])
     
+    #koikoiするかどうか
+    UnivInfo['koikoi'] = observation['koikoi']
+    #自分のターンかどうか
+    UnivInfo['select_time'] = observation['Turn']
+
     return UnivInfo
 
+
+ #obsetvationテスト用
+observation = {}
+observation['round'] = 1
+observation['field_card'] = [[1,1],[1,2],[1,3],[1,4],[2,1],[2,2],[2,3],[2,4]]
+observation['your_hand'] = [[3,1],[3,2],[3,3],[3,4],[4,1],[4,2],[4,3],[4,4]]
+observation['opponent_hand'] = [[5,1],[5,2],[5,3],[5,4],[6,1],[6,2],[6,3],[6,4]]
+observation['your_pile'] = [[7,1],[7,2],[7,3],[7,4],[8,1],[8,2],[8,3],[8,4]]
+transInfo(observation)
 
